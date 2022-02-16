2385b26a8b018de3b27046340607c3f35415ead4
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-16 16:13:33 -0500

    Finished setting up GET and POST routes.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,20 +1,43 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("Get All Tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    // makes the req.params.id variable taskID
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      // deals with ids of the right length that do not exist
+      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    // deals with ids that are not the right length
+    res.status(500).json({ msg: error });
+  }
 };
+
 const updateTask = (req, res) => {
   res.send("Update Task");
 };
+
 const deleteTask = (req, res) => {
   res.send("Delete Task");
 };
