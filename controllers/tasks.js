223c01a897ecf419c912a0538b98c11c2f757f9c
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-16 18:35:10 -0500

    Refactored controllers with an async wrapper.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,81 +1,58 @@
 const Task = require("../models/Task");
+const asyncWrapper = require("../middleware/async");
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    // there are many options for a response
-    res.status(200).json({ tasks }); // sends all tasks
-    // res.status(200).json({ tasks, amount:tasks.length }); sends a certain number of tasks
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks }); // sends all tasks
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+
+  if (!task) {
+    // deals with ids of the right length that do not exist
+    return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
   }
-};
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  res.status(200).json({ task });
+});
+
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    // always returns the new value
+    new: true,
+    // runs the set validators for the schema
+    runValidators: true,
+    // to make it a put to replace and not just change, need to have overwrite: true
+  });
+
+  if (!task) {
+    return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
   }
-};
 
-const getTask = async (req, res) => {
-  try {
-    // makes the req.params.id variable taskID
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID });
+  res.status(200).json({ task });
+});
 
-    if (!task) {
-      // deals with ids of the right length that do not exist
-      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
-    }
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
 
-    res.status(200).json({ task });
-  } catch (error) {
-    // deals with ids that are not the right length
-    res.status(500).json({ msg: error });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
   }
-};
-
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      // always returns the new value
-      new: true,
-      // runs the set validators for the schema
-      runValidators: true,
-      // to make it a put to replace and not just change, need to have overwrite: true
-    });
-
-    if (!task) {
-      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-
-    if (!task) {
-      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
-    }
-    // For delete, it does not really matter what you send back,
-    // since you refresh the entire thing
-    // Only 200 status matters
-    res.status(200).json({ task });
-    // res.status(200).json({ task: null, status: "success" })
-    // res.status(200).send()
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+  // For delete, it does not really matter what you send back,
+  // since you refresh the entire thing
+  // Only 200 status matters
+  res.status(200).json({ task });
+  // res.status(200).json({ task: null, status: "success" })
+  // res.status(200).send()
+});
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
