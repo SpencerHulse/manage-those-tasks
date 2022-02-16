fa0c348ac1c272f6a3a053487423afbb12b40db9
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-16 16:32:16 -0500

    Finished creating the DELETE and PATCH routes.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -34,12 +34,40 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      // always returns the new value
+      new: true,
+      // runs the set validators for the schema
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with the ID: ${taskID}` });
+    }
+    // For delete, it does not really matter what you send back,
+    // since you refresh the entire thing
+    // Only 200 status matters
+    res.status(200).json({ task });
+    // res.status(200).json({ task: null, status: "success" })
+    // res.status(200).send()
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
