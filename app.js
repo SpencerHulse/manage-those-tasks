3a41282ef91b192d5406fe43c55475cd90fc8b0a
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-16 19:17:49 -0500

    Refactored and added custom errors.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasksRoutes = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -13,13 +14,14 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasksRoutes);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3001;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     console.log("Connected to the DB...");
-    app.listen(PORT, (req, res) => {
+    app.listen(PORT, () => {
       console.log("The server is listening on port 3001...");
     });
   } catch (err) {
